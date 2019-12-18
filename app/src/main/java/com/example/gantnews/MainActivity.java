5b20696ca55e2e0f760b6f3570dc67f102f9c800
George <https://github.com/georgegant>
2019-12-18 17:43:50 +0200

    + Error Handling Restrofit

--- app/src/main/java/com/example/gantnews/MainActivity.java
+++ app/src/main/java/com/example/gantnews/MainActivity.java
@@ -19,7 +19,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.support.v7.widget.SearchView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -45,6 +47,11 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
     private String TAG = MainActivity.class.getSimpleName();
     private TextView topHeadline;
     private SwipeRefreshLayout swipeRefreshLayout;
+    private RelativeLayout errorLayout;
+    private ImageView errorImage;
+    private TextView errorTitle, errorMessage;
+    private Button btnRetry;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,10 +71,17 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
 
         onLoadingSwipeRefresh("");
 
+        errorLayout = findViewById(R.id.errorLayout);
+        errorImage = findViewById(R.id.errorImage);
+        errorTitle = findViewById(R.id.errorTitle);
+        errorMessage = findViewById(R.id.errorMessage);
+        btnRetry = findViewById(R.id.btnRetry);
+
     }
 
     public void LoadJson(final String keyword){
 
+        errorLayout.setVisibility(View.GONE);
         topHeadline.setVisibility(View.INVISIBLE);
         swipeRefreshLayout.setRefreshing(true);
 
@@ -105,9 +119,25 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
                     swipeRefreshLayout.setRefreshing(false);
 
                 } else {
+
                     topHeadline.setVisibility(View.INVISIBLE);
                     swipeRefreshLayout.setRefreshing(false);
-                    Toast.makeText(MainActivity.this, "No Result!", Toast.LENGTH_SHORT).show();
+
+                    String errorCode;
+                    switch (response.code()){
+                        case 404:
+                            errorCode = "Not Found";
+                            break;
+                        case 500:
+                            errorCode = "Server Broken";
+                            break;
+                        default:
+                            errorCode = "Unknown Error";
+                            break;
+                    }
+
+                    showErrorMessage(R.drawable.no_result, "No Result", "Please Try Again!\n" + errorCode);
+
                 }
 
             }
@@ -117,6 +147,7 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
 
                 topHeadline.setVisibility(View.INVISIBLE);
                 swipeRefreshLayout.setRefreshing(false);
+                showErrorMessage(R.drawable.no_result, "Ooos..", "Network failure! Please, Try Again\n" + t.toString());
 
             }
         });
@@ -205,6 +236,25 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
                     }
                 }
         );
+    }
+
+    private void showErrorMessage(int imageView, String title, String message){
+
+        if (errorLayout.getVisibility() == View.GONE){
+            errorLayout.setVisibility(View.VISIBLE);
+        }
+
+        errorImage.setImageResource(imageView);
+        errorTitle.setText(title);
+        errorMessage.setText(message);
+
+        btnRetry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLoadingSwipeRefresh("");
+            }
+        });
 
     }
+
 }
