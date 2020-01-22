07391d961c28e75b294b46f79444bc1d64713fa7
George <https://github.com/georgegant>
2020-01-22 14:57:55 +0200

    + Error Handling Restrofit

---
+++ app/src/main/java/com/example/gantnews/SplashScreen.java
@@ -0,0 +1,33 @@
+package com.example.gantnews;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static java.lang.Thread.sleep;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+
+                    sleep(1500);
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        });thread.start();
+
+    }
+}
